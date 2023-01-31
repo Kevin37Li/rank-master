8d8950f7a91f4482775f3506e7dafe609d370961
jchao537 <jchao537@github.com>
2023-01-30 20:53:15 -0800

    basic front end quick sort

---
+++ src/lists.js
@@ -0,0 +1,33 @@
+export const mlb_list = [ "ARI", "COL", "HOU", "LAA", "LAD", "OAK", "SD", "SF", "SEA", "TEX" ];
+
+export const num_list = [30, 10, 20, 40, 0, 60, 50]
+
+export const zutomayo_list = [
+    "\"Byōshin o Kamu\" (秒針を噛む)",
+    "\"Nōriue no Cracker\" (脳裏上のクラッカー)",
+    "\"Humanoid\" (ヒューマノイド)",
+    "\"Mabushii DNA Dake\" (眩しいDNAだけ)",
+    "\"Seigi\" (正義)",
+    "\"Kettobashita Mōfu\" (蹴っ飛ばした毛布)",
+    "\"Obenkyō Shitoite yo\" (お勉強しといてよ)",
+    "\"Darken\" (暗く黒く)",
+    "\"Hunch Gray\" (勘ぐれい)",
+    "\"Can't Be Right\" (正しくなれない)",
+    "\"Inside Joke\" (あいつら全員同窓会)",
+    "\"Stay Foolish\" (ばかじゃないのに)",
+    "\"Mirror Tune\" (ミラーチューン)",
+    "\"Blush\" (消えてしまいそうです)",
+    "\"Time Left\" (残機)",
+    "\"Kira Killer\" (綺羅キラー)\n(featuring Mori Calliope)",
+]
+
+export const jojo_list = [
+    "Part 1: Phantom Blood",
+    "Part 2: Battle Tendency",
+    "Part 3: Stardust Crusaders",
+    "Part 4: Diamond Is Unbreakable",
+    "Part 5: Golden Wind",
+    "Part 6: Stone Ocean",
+    "Part 7: Steel Ball Run",
+    "Part 8: JoJolion",
+]
