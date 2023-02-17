60f9dade3615e1e0ed929ac6746f458b63589c88
Ethan Chen <ejychen296@gmail.com>
2023-02-16 23:39:51 -0800

    home page initial

--- rankMaster/react-frontend/src/pages/home.js
+++ rankMaster/react-frontend/src/pages/home.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import './home.css';
+import image from './5039684.jpg'
 
 function Home() {
     return (
-        <h1>this is the homepage</h1>
+        <div className="home">
+            {/*<img className="back" src={image}></img>*/}
+            <div className="welcome">
+                <h1>Welcome to Rank Master!</h1>
+            </div>
+            <div className="about">
+                <p>Have you ever been asked what your favorite restaurant in Sawtelle was, but couldn't decide? Or maybe you </p>
+            </div>
+        </div>
     );
 }
 
