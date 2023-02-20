68455667b05bbf158cdb81bb578f0152ddb3d92f
Ethan Chen <ejychen296@gmail.com>
2023-02-19 16:22:16 -0800

    styling additions

--- rankMaster/react-frontend/src/pages/home.js
+++ rankMaster/react-frontend/src/pages/home.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './home.css';
-import image from './5039684.jpg'
 
 function Home() {
     return (
-        <div className="home">
-            {/*<img className="back" src={image}></img>*/}
-            <div className="welcome">
-                <h1>Welcome to Rank Master!</h1>
-            </div>
-            <div className="about">
-                <p>Have you ever been asked what your favorite restaurant in Sawtelle was, but couldn't decide? Or maybe you </p>
+        <div className="homescreen">
+            <div className="home">
+                <div className="welcome">
+                    <h1>Welcome to Rank Master!</h1>
+                </div>
+                <div className="about">
+                    <p>Have you ever been asked what your favorite restaurant in Sawtelle was, but couldn't decide? Or maybe you </p>
+                </div>
             </div>
         </div>
     );
