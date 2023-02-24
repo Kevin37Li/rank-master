c980fe6e0715e9d868ea640c65cf2bacb37821a2
Ethan Chen <ejychen296@gmail.com>
2023-02-23 16:29:39 -0800

    initial categories styling

--- rankMaster/react-frontend/src/pages/categories.js
+++ rankMaster/react-frontend/src/pages/categories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import {list_of_lists} from "../lists"
 import { useNavigate } from "react-router-dom";
+import './categories.css';
 
 function Categories() {
     // const [items, setItems] = React.useState([]);
@@ -21,57 +22,61 @@ function Categories() {
     let others_items = list_of_lists;
 
     return (
-        <div>
-            <h1>this is the categories</h1>
-            <div>
-                <h2>Movies</h2>
-                <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-                    {movie_items.map((item) => (
-                        <button onClick={() => routeChange(`/myApp/lists/rank/${item.id}`)} style={{width: '160px',}}>
-                            {item.title}
-                        </button>
-                    ))}
-                </ScrollMenu>
-            </div>
-            <div>
-                <h2>Music</h2>
-                <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-                    {music_items.map((item) => (
-                        <button onClick={() => routeChange(`/myApp/lists/rank/${item.id}`)} style={{width: '160px',}}>
-                            {item.title}
-                        </button>
-                    ))}
-                </ScrollMenu>
-            </div>
-            <div>
-                <h2>Sports</h2>
-                <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-                    {sport_items.map((item) => (
-                        <button onClick={() => routeChange(`/myApp/lists/rank/${item.id}`)} style={{width: '160px',}}>
-                            {item.title}
-                        </button>
-                    ))}
-                </ScrollMenu>
-            </div>
-            <div>
-                <h2>TV</h2>
-                <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-                    {tv_items.map((item) => (
-                        <button onClick={() => routeChange(`/myApp/lists/rank/${item.id}`)} style={{width: '160px',}}>
-                            {item.title}
-                        </button>
-                    ))}
-                </ScrollMenu>
-            </div>
-            <div>
-                <h2>Others</h2>
-                <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-                    {others_items.map((item) => (
-                        <button onClick={() => routeChange(`/myApp/lists/rank/${item.id}`)} style={{width: '160px',}}>
-                            {item.title}
-                        </button>
-                    ))}
-                </ScrollMenu>
+        <div className="containercat">
+            <div className="categoriesscreen">
+                <div className="categories">
+                    <h2>Categories</h2>
+                    <div>
+                        <h3>Movies</h3>
+                        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+                            {movie_items.map((item) => (
+                                <button onClick={() => routeChange(`/myApp/lists/rank/${item.id}`)} style={{width: '160px',}}>
+                                    {item.title}
+                                </button>
+                            ))}
+                        </ScrollMenu>
+                    </div>
+                    <div>
+                        <h3>Music</h3>
+                        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+                            {music_items.map((item) => (
+                                <button onClick={() => routeChange(`/myApp/lists/rank/${item.id}`)} style={{width: '160px',}}>
+                                    {item.title}
+                                </button>
+                            ))}
+                        </ScrollMenu>
+                    </div>
+                    <div>
+                        <h3>Sports</h3>
+                        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+                            {sport_items.map((item) => (
+                                <button onClick={() => routeChange(`/myApp/lists/rank/${item.id}`)} style={{width: '160px',}}>
+                                    {item.title}
+                                </button>
+                            ))}
+                        </ScrollMenu>
+                    </div>
+                    <div>
+                        <h3>TV</h3>
+                        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+                            {tv_items.map((item) => (
+                                <button onClick={() => routeChange(`/myApp/lists/rank/${item.id}`)} style={{width: '160px',}}>
+                                    {item.title}
+                                </button>
+                            ))}
+                        </ScrollMenu>
+                    </div>
+                    <div>
+                        <h3>Others</h3>
+                        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+                            {others_items.map((item) => (
+                                <button onClick={() => routeChange(`/myApp/lists/rank/${item.id}`)} style={{width: '160px',}}>
+                                    {item.title}
+                                </button>
+                            ))}
+                        </ScrollMenu>
+                    </div>
+                </div>
             </div>
         </div>
     );
